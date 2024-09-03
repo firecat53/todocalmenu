1a27240ee93e10207b2d5c4223d21e841f0b96b1
Scott Hansen <tech@firecat53.net>
2024-09-03 14:56:15 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Todotxtmenu";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = ["x86_64-linux" "i686-linux" "aarch64-linux"];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system:
+        f rec {
+          pkgs = nixpkgs.legacyPackages.${system};
+          commonPackages = builtins.attrValues {
+            inherit
+              (pkgs)
+              delve
+              gnumake
+              go
+              go-outline
+              gocode-gomod
+              godef
+              golint
+              gopkgs
+              gopls
+              gotools
+              ;
+          };
+        });
+  in {
+    devShells = forAllSystems ({
+      pkgs,
+      commonPackages,
+    }: {
+      default = pkgs.mkShell {
+        packages = commonPackages ++ [pkgs.pandoc];
+        shellHook = ''
+        '';
+      };
+    });
+  };
+}
